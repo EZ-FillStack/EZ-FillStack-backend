7324ba32621cafb5649e9fac85abd4f9b5ef29bb
LHI020912 <gpdlsrhd@gmail.com>
2026-02-27 01:05:02 +0900

    fix: bookmark API & Execption 처리 수정

--- backend/src/main/java/com/ezwell/backend/domain/bookmark/exception/AlreadyBookmarkedException.java
+++
@@ -1,7 +0,0 @@
-package com.ezwell.backend.domain.bookmark.exception;
-
-public class AlreadyBookmarkedException extends RuntimeException  {
-	public AlreadyBookmarkedException(String massge) {
-	}
-
-}
