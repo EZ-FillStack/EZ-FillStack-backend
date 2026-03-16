98f9a4cb3594b7cac0b460d3640ac179270df56f
LHI020912 <gpdlsrhd@gmail.com>
2026-03-16 23:43:22 +0900

    fix(inquiry): 예외 처리 정정

--- backend/src/main/java/com/ezwell/backend/global/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ezwell/backend/global/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.ezwell.backend.domain.bookmark.exception.BookmarkedException;
 import com.ezwell.backend.domain.event.exception.EventNotFoundException;
 import com.ezwell.backend.domain.inquiry.exception.InquiryException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -32,7 +33,7 @@ public class GlobalExceptionHandler {
     }
     
     @ExceptionHandler(InquiryException.class)
-    public ResponseEntity<ErrorResponse> InquirtException(InquiryException e){
+    public ResponseEntity<ErrorResponse> InquiryException(InquiryException e){
     	return ResponseEntity.status(400).body(new ErrorResponse("INQUIRY_ERROR", e.getMessage()));
     	// 400 : 유효성 검사 실패나 로직 오류
     }
@@ -46,13 +47,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> ApplicationEventException(ApplicationEventException e){
     	return ResponseEntity.status(400).body(new ErrorResponse("APPLICATION_ERROR",e.getMessage()));
     }
-
+    
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handlerAllException(Exception e) {
         // 로그를 남겨서 나중에 디버깅할 수 있게 함
         return ResponseEntity.status(500).body(new ErrorResponse("INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다."));
     }
-
+    
     @ExceptionHandler(org.springframework.security.access.AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
         return ResponseEntity.status(403).body(new ErrorResponse("FORBIDDEN", "해당 작업에 대한 권한이 없습니다."));
